4864dfc1678d4aa900d9fb6435840b86704ed797
FelipeOliver00 <Felipecastro0208@hotmail.com>
2021-10-02 12:30:03 -0300

    Created interfaces

---
+++ src/main/java/com/access/userservice/repo/UserRepo.java
@@ -0,0 +1,9 @@
+package com.access.userservice.repo;
+
+import com.access.userservice.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepo extends JpaRepository <User, Long>{
+    User findByUsername(String username);
+
+}
