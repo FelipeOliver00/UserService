fd90a01dd91710e72d659a8f145c5f7e7d7840da
FelipeOliver00 <Felipecastro0208@hotmail.com>
2021-10-02 14:29:23 -0300

    Defined properties

---
+++ src/main/java/com/access/userservice/api/UserResource.java
@@ -0,0 +1,23 @@
+package com.access.userservice.api;
+
+import com.access.userservice.domain.User;
+import com.access.userservice.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+public class UserResource {
+    private final UserService userService;
+
+    @GetMapping("/users")
+    public ResponseEntity<List<User>>getUsers() {
+        return ResponseEntity.ok().body(userService.getUsers());
+    }
+}
